0823caf7ef9093fe5fd8ad47e5ab0aed945c1bfc
CalebWheelock <38189806+CalebWheelock@users.noreply.github.com>
2018-04-17 16:56:29 -0400

    Add files via upload
    Made some changes to the sailco data file to adapt it to ours. I used the data for a whole week instead of just each day, I am not sure yet what will work best. The py file is just the sailco.py file

---
+++ ProjectTransport.py
@@ -0,0 +1,32 @@
+#Caleb Wheelock
+
+from pyomo.environ import*
+#Declare Model
+model=AbstractModel()
+
+#sets
+model.SUPPLIES = Set() #supply nodes
+model.DEMANDS = Set() #demand nodes
+
+#parameters
+model.s = Param(model.SUPPLIES) #supply avail at each node
+model.d = Param(model.DEMANDS) #demand req at each node
+model.c = Param(model.SUPPLIES, model.DEMANDS) #shipping cost from supply point i to demand point j
+
+#decision variables: x[ij] = amount shipped from supply point i to demand point j
+model.x = Var(model.SUPPLIES, model.DEMANDS, within = NonNegativeReals)
+
+#objective function: min cost to meet demands
+def objective_rule(model):
+    return sum(model.c[i,j] * model.x[i,j] for i in model.SUPPLIES for j in model.DEMANDS)
+model.minCost = Objective(rule=objective_rule, sense=minimize)
+
+#Supply Constraint
+def supply_rule(model,i):
+    return (sum(model.x[i,j] for j in model.DEMANDS) == model.s[i] )
+model.supplyConstraints = Constraint(model.SUPPLIES, rule=supply_rule)
+
+#Demand Constraint
+def demand_rule(model,j):
+    return (sum(model.x[i,j] for i in model.SUPPLIES) == model.d[j])
+model.demandConstraints = Constraint(model.DEMANDS, rule=demand_rule)
