6090bf9ed7458456b698efa557d6e51ebffec153
CalebWheelock <38189806+CalebWheelock@users.noreply.github.com>
2018-04-20 14:20:45 -0400

    Update ProjectTransport.py
    When running each of the three data files I have posted make sure to change the name of the data file in the run section of the code.

--- ProjectTransport.py
+++ ProjectTransport.py
@@ -14,7 +14,6 @@ model.PrestigeSUPPLIES = Set() #prestige supply nodes
 model.s = Param(model.SUPPLIES) #supply avail at each node
 model.d = Param(model.DEMANDS) #demand req at each node
 model.c = Param(model.SUPPLIES, model.DEMANDS) #shipping cost from supply point i to demand point j
-model.t = Param(model.SUPPLIES) #time available for each worker
 
 #decision variables: x[ij] = amount shipped from supply point i to demand point j
 model.x = Var(model.SUPPLIES, model.DEMANDS, within = NonNegativeReals)
@@ -34,27 +33,35 @@ def demand_rule(model,j):
     return (sum(model.x[i,j] for i in model.SUPPLIES) == model.d[j])
 model.demandConstraints = Constraint(model.DEMANDS, rule=demand_rule)
 
-#Full Time 8 Hour Block
-def FullTime_rule(model,i,j):
-    return model.x[i,j] + model.x[i,j+1] == 2
-model.FTReq = Constraint(model.FulltimeSUPPLIES,model.DEMANDS, rule=FullTime_rule)
-
-#Full Time 8 Hour Block
-#def FullTime_rule(model,i):
-#    if model.t[i] == 'FT':
-#        return model.x[i,j] + model.x[i,j+1] == 2
-#    else:
-#        pass
-#model.FTReq = Constraint(model.SUPPLIES, rule=FullTime_rule)
-
-#Birthday Requirement
-def BDay_rule(model):
-    return sum(model.x[i,'BP'] for i in model.PrestigeSupplies) >= 1
-model.BDayReq = Constraint(model.PrestigeSUPPLIES, rule=BDay_rule)
+##Full Time 8 Hour Block
+#def FullTime1_rule(model,i):
+#    return model.x[i,'FG'] + model.x[i,'BP'] >= 56
+#model.FTReq1 = Constraint(model.FulltimeSUPPLIES, rule=FullTime1_rule)
+#
+#def FullTime2_rule(model,i):
+#    return model.x[i,'BP'] + model.x[i,'CL'] >= 56
+#model.FTReq2 = Constraint(model.FulltimeSUPPLIES, rule=FullTime2_rule)
+#
+#def FullTime3_rule(model,i):
+#    return model.x[i,'CL'] + model.x[i,'AG'] >= 56
+#model.FTReq3 = Constraint(model.FulltimeSUPPLIES, rule=FullTime3_rule)
+#
+##Full Time 8 Hour Block
+##def FullTime_rule(model,i):
+##    if model.t[i] == 'FT':
+##        return model.x[i,j] + model.x[i,j+1] == 2
+##    else:
+##        pass
+##model.FTReq = Constraint(model.SUPPLIES, rule=FullTime_rule)
+#
+##Birthday Requirement
+#def BDay_rule(model):
+#    return sum(model.x[i,'BP'] for i in model.PrestigeSUPPLIES) >= 28
+#model.BDayReq = Constraint(rule=BDay_rule)
 
 '**********************************************Running the Model****************************************'
 data = DataPortal()
-data.load(filename="ProjectData.dat", model=model)
+data.load(filename="ProjectData3.dat", model=model)
 
 optimizer = SolverFactory("glpk")
 instance = model.create_instance(data)
