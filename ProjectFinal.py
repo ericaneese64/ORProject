c845d99729e5f029f5409c9176ba6d78e5945a3b
CalebWheelock <38189806+CalebWheelock@users.noreply.github.com>
2018-04-23 15:57:09 -0400

    Update ProjectFinal.py

--- ProjectFinal.py
+++ ProjectFinal.py
@@ -1,28 +1,27 @@
-#Caleb Wheelock
+#Caleb Wheelock, Jacob Smythe, Michael Halligan, Erica Neese
 
 from pyomo.environ import*
 #Declare Model
 model=AbstractModel()
 
 #sets
-model.SUPPLIES = Set() #supply nodes
-model.DEMANDS = Set() #demand nodes
+model.SUPPLIES = Set() #supply nodes: Each worker
+model.DEMANDS = Set() #demand nodes: Each shift for part and full time workers
 
 #parameters
-model.s = Param(model.SUPPLIES) #supply avail at each node
-model.d = Param(model.DEMANDS) #demand req at each node
-model.c = Param(model.SUPPLIES, model.DEMANDS) #shipping cost from supply point i to demand point j
-#model.p = Param(model.SUPPLIES)
+model.s = Param(model.SUPPLIES) #supply of hours available for each worker
+model.d = Param(model.DEMANDS) #demand required for each shift
+model.c = Param(model.SUPPLIES, model.DEMANDS) #wage per hour for each worker for a given shift
 
-#decision variables: x[ij] = amount shipped from supply point i to demand point j
+#decision variables: x[ij] = amount of hours each worker i works for shift j
 model.x = Var(model.SUPPLIES, model.DEMANDS, within = NonNegativeReals)
 
-#objective function: min cost to meet demands
+#objective function: minimize the daily cost of labor for each worker i and shift j
 def objective_rule(model):
     return sum(model.c[i,j] * model.x[i,j] for i in model.SUPPLIES for j in model.DEMANDS)
 model.minCost = Objective(rule=objective_rule, sense=minimize)
 
-#Supply Constraint
+#Supply Constraint: The total 
 def supply_rule(model,i):
     return (sum(model.x[i,j] for j in model.DEMANDS) == model.s[i] )
 model.supplyConstraints = Constraint(model.SUPPLIES, rule=supply_rule)
